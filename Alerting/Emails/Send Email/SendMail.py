13719b63a56ca8afb2cf4693e5cd4394ff14988b
AurelienDHI <33960111+AurelienDHI@users.noreply.github.com>
2018-03-03 14:57:52 +0100

    Update SendMail.py

--- Alerting/Emails/Send Email/SendMail.py
+++ Alerting/Emails/Send Email/SendMail.py
@@ -1,87 +1,71 @@
-import os
+import clr
+clr.AddReference("DHI.Solutions.Generic")
+from DHI.Solutions.Generic import *
 
-import smtplib
+from System.IO import Path, Directory
 
-import string
+from System.Net import *
+from System.Net.Mail import *
+from System.Net.Security import *
 
-import email.utils
-
-from smtplib import *
-
-
-
-from email.utils import *
-
-from email.mime.multipart import MIMEMultipart,MIMEBase
-
-from email.mime.text import MIMEText
-
-
-
-from email import Encoders
-
-from DHI.Solutions.TimeseriesManager.Tools.Processing import *
-
-
-
-def _SendMail(emails, subject, msgtxt, msghtml):
-
-        # mail_user = r'myemail@my.smtpserver.com'
-
-        mail_user = r'myemail@gmail.com'
-
-       mail_pwd = r'mypassword'
-
-
-
-    msg = MIMEMultipart()
-
-    msg["Subject"] = subject
-
-    msg['Date']    = formatdate(localtime=True)
-
-    if msghtml != "" :
-
-        msg.attach(MIMEText(msghtml, 'html'));
-
-    if msgtxt != "":
-
-        msg.attach(MIMEText(msgtxt, 'plain'));
-
-
-
+def customCertValidation(s, certificate, chain, sslPolicyErrors):
+    return True;
+    
+def _SendMail(emails, msgsubj, msghtml, filename = ""):
+    print "_SendMail " + emails
+    
+    mail_user = r'andersklinting@gmail.com'
+    mail_pwd = r'password'    
+    mail_server = 'smtp.gmail.com'
+    mail_port = 587
+    mail_ssl = True
+    
     try:
-
-        # sending via local smtp
-
-        # smtpserver = smtplib.SMTP("my.stmpserver.com")
-
-
-
-        # sending via gmail
-
-        smtpserver = smtplib.SMTP("smtp.gmail.com",587)
-
-        smtpserver.ehlo()
-
-        smtpserver.starttls()
-
-        smtpserver.ehlo
-
-        smtpserver.login(mail_user, mail_pwd)
-
-
-
-        smtpserver.sendmail(mail_user, emails.split(";") + [mail_user], msg.as_string())
-
+        ServicePointManager.ServerCertificateValidationCallback = RemoteCertificateValidationCallback(customCertValidation);
+        
+        message = MailMessage()
+        message.From = MailAddress(mail_user)
+        for em in emails.split(";"):
+            message.To.Add(MailAddress(em.strip()));
+        message.Subject = msgsubj
+        if msghtml != "" :
+            message.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(msghtml, None, "text/html"));
+        if filename != "":            
+            att = Attachment(filename)
+            message.Attachments.Add(att)
+            
+        client = SmtpClient(mail_server, mail_port);
+        client.EnableSsl = mail_ssl
+        client.UseDefaultCredentials = False
+        client.Credentials = NetworkCredential(mail_user, mail_pwd)
+        
+        client.Send(message);
+    except Exception as e:
+        print str(e)
 
 
-        smtpserver.quit()
+def _GetDoc(docPath):
+    
+    docMgr = app.Modules.Get("Document Manager")
+    doc = docMgr.DocumentList.Fetch(docPath)
+    
+    # make unique dir and file under the application temprorary dir
+    dir = Path.Combine(DssPath.GetTemporaryDirectory(), Path.GetRandomFileName())
+    Directory.CreateDirectory(dir)
+    filename = Path.Combine(dir, doc.Name)
+    
+    docMgr.DocumentList.Export(doc, filename, True)
 
+    return filename
 
+def TestEmailDocument():
+    """
+    <Script>
+    <Author>ANK</Author>
+    <Description>get a document and send it by email as attachment</Description>
+    </Script>
+    """
 
-            except Exception, e:
-
-        errorMsg = "Unable to send email. Error: %s" % str(e)
-
-        print errorMsg
+    filename = _GetDoc("/AUG/EUR_MIKEPoweredByDHI_Pricelist_Corporate_Rel2017_21June2017.pdf")
+    _SendMail("ank@dhigroup.com", "test send doc subject", "this is a test body, <b>HTML tags</b> are possible", filename)
+    print "done"
