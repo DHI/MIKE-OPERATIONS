5b1094ad9b4993d74132d1d10de52d4a41a6af4b
AurelienDHI <33960111+AurelienDHI@users.noreply.github.com>
2018-03-13 13:16:04 +0100

    Create ModifyScenarioDates.py

---
+++ Modeling/ModifyScenarioDates/ModifyScenarioDates.py
@@ -0,0 +1,32 @@
+from System import DateTime
+
+def UpdateTOF():
+
+    """
+    <Script>
+    <Author>ANK</Author>
+    <Description>This script modifies scenario dates </Description>
+    </Script>
+    """
+
+    scenarioFullPath = "/Group of cali/cali/Scenario of cali"
+    TOF = DateTime(2014,2,1)
+    SOS = TOF.AddDays(-1)
+    EOS = TOF.AddDays(1)
+
+    scmgr = app.Modules.Get("Scenario Manager")
+ 
+    scenario = scmgr.ScenarioList.Fetch(scenarioFullPath);
+
+    # first make sure we can set the dates without conflict (must hav SOS<=TOF<=EOS)
+    scenario.SimulationStartDate = DateTime.MinValue
+    scenario.SimulationTimeOfForecast = DateTime.MinValue
+    scenario.SimulationEndDate = DateTime.MinValue
+
+    # then set the dates
+    scenario.SimulationEndDate = EOS
+    scenario.SimulationTimeOfForecast = TOF
+    scenario.SimulationStartDate = SOS
+
+    # write changes to the database
+   scmgr.ScenarioList.Update (scenario);
