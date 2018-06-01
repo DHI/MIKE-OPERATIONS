5f7259c9581ebbbf9dc44a7a7e7d5f20178381db
AurelienDHI <33960111+AurelienDHI@users.noreply.github.com>
2018-06-01 13:25:57 +0200

    Update ImportSpreadsheet.py

--- DataImport/Spreadsheet/ImportSpreadsheet.py
+++ DataImport/Spreadsheet/ImportSpreadsheet.py
@@ -1,1 +1,16 @@
 
+def ImportSpreadsheet(spreadsheetPath, spreadsheetDirectory):
+    """
+    <Script>
+    <Author>AUG</Author>
+    <Description>Import a spreadsheet from a folder on disk</Description>
+    <Parameters>
+    <Parameter name="spreadsheetPath" type="string">Full path to the spreadsheet</Parameter>
+    <Parameter name="spreadsheetDirectory" type="string">directory path on disk</Parameter>
+    </Parameters>
+    </Script>
+    """
+    
+    # Get a reference to the Time series Manager
+    ssMgr = app.Modules.Get('Spreadsheet Manager')
+    ssMgr.SpreadsheetList.Import(spreadsheetDirectory, spreadsheetPath);
