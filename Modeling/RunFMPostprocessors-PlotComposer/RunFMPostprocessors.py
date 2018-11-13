dbc30af45586286d9a950105c2ac137c98520356
AurelienDHI <33960111+AurelienDHI@users.noreply.github.com>
2018-11-13 08:43:13 +0100

    Update RunFMPostprocessors.py

--- Modeling/RunFMPostprocessors-PlotComposer/RunFMPostprocessors.py
+++ Modeling/RunFMPostprocessors-PlotComposer/RunFMPostprocessors.py
@@ -11,25 +11,24 @@ def Lauch_MIKE21FM_Postprocessors():
     <Description>This script run several MIKE ZERO post processing tools (DataStatisticsFM, DataExtractionFM, DataVerticalAveragingFM as well as plot composer)</Description>
     </Script>
     """
+    bin = r'C:\Program Files (x86)\DHI\2017\bin\x64'
+    DataStatisticsFM = bin + r'\DataStatisticsFM.exe'
+    DataExtractionFM = bin + r'\DataExtractionFM.exe'
+    DataVerticalAveragingFM = bin + r'\DataVerticalAveragingFM.exe'
+    MZPlotComposer = bin + r'\MzPlotCompApp.exe'
     
-	bin = r'C:\Program Files (x86)\DHI\2017\bin\x64'
-	DataStatisticsFM = bin + r'\DataStatisticsFM.exe'
-	DataExtractionFM= bin + r'\DataExtractionFM.exe'
-	DataVerticalAveragingFM = bin + r'\DataVerticalAveragingFM.exe'
-	MZPlotComposer = bin + r'\MzPlotCompApp.exe'
-	
-	MT= r'c:\WORK\Simulations\ResultsMT\Analyse'
-	
-	process = Process.Start(DataExtractionFM, MT + '\Extract_MT-3D_SSC.dxfm');
-	process.WaitForExit();
-
-	Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_-100m.pfs')
-	Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_Fond.pfs')
-	process = Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_Max_Colonne.pfs')
-	process.WaitForExit();
-	
-	Process.Start(DataStatisticsFM, MT + '\Extract_SSC_Max_Temporel_Max_Colonne.pfs');
-	process.WaitForExit();
-	
-	Process.Start(MZPlotComposer, MT + '\MyPlot.plc' +  ' -printtofile ' + MT + '\MyPlot.png');
-	process.WaitForExit();
+    MT= r'c:\WORK\Simulations\ResultsMT\Analyse'
+    
+    process = Process.Start(DataExtractionFM, MT + '\Extract_MT-3D_SSC.dxfm');
+    process.WaitForExit();
+    
+    Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_-100m.pfs')
+    Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_Fond.pfs')
+    process = Process.Start(DataVerticalAveragingFM, MT + '\Extract_SSC_Max_Colonne.pfs')
+    process.WaitForExit();
+    
+    Process.Start(DataStatisticsFM, MT + '\Extract_SSC_Max_Temporel_Max_Colonne.pfs');
+    process.WaitForExit();
+    
+    Process.Start(MZPlotComposer, MT + '\MyPlot.plc' +  ' -printtofile ' + MT + '\MyPlot.png');
+    process.WaitForExit();
