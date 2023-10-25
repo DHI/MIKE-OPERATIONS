5580c1e70a15101e3291a4fd027a27bafe73efb9
kthage <32189603+kthage@users.noreply.github.com>
2023-10-25 08:19:21 +0200

    Create RemovePreparedScripts.py

---
+++ DataHandling/Scripts/RemovePreparedScripts.py
@@ -0,0 +1,35 @@
+import sys
+import clr
+clr.AddReference('DHI.Solutions.Generic')
+import DHI.Solutions.Generic
+
+def RemovePreparedScripts(scriptPath):
+    """
+    <Script>
+    <Author>admin</Author>
+    <Description>Script to remove associated prepared scripts (parameters stored) for a script.</Description>
+    <Parameters>
+    <Parameter name="scriptPath" type="string">Full path to the script to remove prepared script for.</Parameter>
+    </Parameters>
+    </Script>
+    """
+    
+    if scriptPath == None:
+        raise System.ArgumentException('ScriptPath should be specified')
+        
+    scriptModule = app.Modules.Get('Script Manager')
+    
+    script = scriptModule.ScriptList.Fetch(scriptPath)
+    
+    if script == None:
+        raise System.ArgumentException('A script with the specified path does not exist.')
+    
+    query = DHI.Solutions.Generic.Query()
+    query.Add(DHI.Solutions.Generic.QueryElement("ScriptId", script.Id, DHI.Solutions.Generic.QueryOperator.Eq))
+    associatedPpreparedScripts = scriptModule.PreparedScriptList.Fetch(query);
+    
+    if associatedPpreparedScripts.Count == 0:
+        raise System.ArgumentException('No prepared scripts found for the script.')
+    
+    scriptModule.PreparedScriptList.Delete(associatedPpreparedScripts);
+    pass;
